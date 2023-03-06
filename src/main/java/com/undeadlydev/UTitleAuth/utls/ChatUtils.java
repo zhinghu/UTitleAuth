ec93aa3762e5263d0e3feb5b0ba22b8ce3c58aef
UnDeadlyDev <87789394+UnDeadlyDev@users.noreply.github.com>
2023-03-05 22:21:44 -0300

    fix hex

--- src/main/java/com/undeadlydev/UTitleAuth/utls/ChatUtils.java
+++ src/main/java/com/undeadlydev/UTitleAuth/utls/ChatUtils.java
@@ -1,28 +1,22 @@
 package com.undeadlydev.UTitleAuth.utls;
 
 import java.util.EnumSet;
-import java.util.Locale;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.undeadlydev.UTitleAuth.TitleAuth;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Color;
 import org.bukkit.entity.Player;
 
 public class ChatUtils {
 
-	private static final Pattern REPLACE_ALL_PATTERN = Pattern.compile("(&)?&([0-9a-fk-orA-FK-OR])");
-	
-	private static final Pattern REPLACE_ALL_RGB_PATTERN = Pattern.compile("(&)?&#([0-9a-fA-F]{6})");
-	
+    private static Pattern HEX_PATTERN = Pattern.compile("<#([A-Fa-f0-9]){6}>");
 
     public static String colorCodes(String paramString) {
         String coloredText = paramString;
         if(VersionUtils.getVersion().esMayorIgual(VersionUtils.v1_16)) {
-            coloredText = parseHex(coloredText, EnumSet.allOf(ChatColor.class), true);
+            coloredText = color(coloredText);
         }
         coloredText = ChatColor.translateAlternateColorCodes('&', coloredText);
         return coloredText;
@@ -43,52 +37,16 @@ public class ChatUtils {
         return newString;
     }
 
-    public static String parseHex(String input, Set<ChatColor> supported, boolean rgb) {
-        StringBuffer legacyBuilder = new StringBuffer();
-        Matcher legacyMatcher = REPLACE_ALL_PATTERN.matcher(input);
-        while (legacyMatcher.find()) {
-            boolean isEscaped = (legacyMatcher.group(1) != null);
-            if (!isEscaped) {
-                char code = legacyMatcher.group(2).toLowerCase(Locale.ROOT).charAt(0);
-                for (ChatColor color : supported) {
-                    if (color.getChar() == code)
-                        legacyMatcher.appendReplacement(legacyBuilder, "§$2");
-                }
-            }
-            legacyMatcher.appendReplacement(legacyBuilder, "&$2");
-        }
-        legacyMatcher.appendTail(legacyBuilder);
-        if (rgb) {
-            StringBuffer rgbBuilder = new StringBuffer();
-            Matcher rgbMatcher = REPLACE_ALL_RGB_PATTERN.matcher(legacyBuilder.toString());
-            while (rgbMatcher.find()) {
-                boolean isEscaped = (rgbMatcher.group(1) != null);
-                if (!isEscaped)
-                    try {
-                        String hexCode = rgbMatcher.group(2);
-                        rgbMatcher.appendReplacement(rgbBuilder, parseHexColor(hexCode));
-                        continue;
-                    } catch (NumberFormatException numberFormatException) {}
-                rgbMatcher.appendReplacement(rgbBuilder, "&#$2");
-            }
-            rgbMatcher.appendTail(rgbBuilder);
-            return rgbBuilder.toString();
-        }
-        return legacyBuilder.toString();
-    }
+    private static String color(String coloredText) {
+        Matcher matcher = HEX_PATTERN.matcher(coloredText);
+        while (matcher.find()) {
+            ChatColor hexColor = ChatColor.of(matcher.group().substring(1, matcher.group().length() - 1));
+            String before = coloredText.substring(0, matcher.start());
+            String after = coloredText.substring(matcher.end());
+            coloredText = before + hexColor + after;
+            matcher = HEX_PATTERN.matcher(coloredText);
 
-    public static String parseHexColor(String hexColor) throws NumberFormatException {
-        if (!VersionUtils.getVersion().esMayorIgual(VersionUtils.v1_16))
-            throw new NumberFormatException("Cannot use RGB colors in versions < 1.16");
-        if (hexColor.startsWith("#"))
-            hexColor = hexColor.substring(1);
-        if (hexColor.length() != 6)
-            throw new NumberFormatException("Invalid hex length");
-        Color.fromRGB(Integer.decode("#" + hexColor).intValue());
-        StringBuilder assembledColorCode = new StringBuilder();
-        assembledColorCode.append("§x");
-        for (char curChar : hexColor.toCharArray())
-            assembledColorCode.append('§').append(curChar);
-        return assembledColorCode.toString();
+        }
+        return coloredText;
     }
 }
