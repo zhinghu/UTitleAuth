5e8e552a420479caee1893b7c3b972a83bf714cb
UnDeadlyDev <87789394+UnDeadlyDev@users.noreply.github.com>
2024-06-17 12:58:32 -0400

    support 1.21

--- src/main/java/com/undeadlydev/UTitleAuth/controllers/VersionController.java
+++ src/main/java/com/undeadlydev/UTitleAuth/controllers/VersionController.java
@@ -2,6 +2,7 @@ package com.undeadlydev.UTitleAuth.controllers;
 
 import com.undeadlydev.UTitleAuth.TitleAuth;
 import com.undeadlydev.UTitleAuth.nms.NMSReflectionNew;
+import com.undeadlydev.UTitleAuth.nms.NMSReflectionNewes;
 import com.undeadlydev.UTitleAuth.nms.NMSReflectionOld;
 import com.undeadlydev.UTitleAuth.superclass.NMSReflection;
 import com.undeadlydev.UTitleAuth.enums.Versions;
@@ -13,7 +14,9 @@ public class VersionController {
 
     public VersionController(TitleAuth plugin) {
         this.plugin = plugin;
-        if (Versions.getVersion().esMayorIgual(Versions.v1_16)) {
+        if (Versions.getVersion().esMayorIgual(Versions.v1_20_5)) {
+            this.reflection = new NMSReflectionNewes();
+        } else if (Versions.getVersion().esMayorIgual(Versions.v1_17_1)) {
             this.reflection = new NMSReflectionNew();
         } else {
             this.reflection = new NMSReflectionOld();
